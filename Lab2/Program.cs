814d931170c992585a03c68ce2eed8141bd4809c
Richard Trávníček <89924371+Hadrik@users.noreply.github.com>
2026-02-26 16:43:50 +0100

    Lab 2 vylepseni

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -2,9 +2,20 @@
 
 namespace Lab2;
 
-internal class Program
+internal partial class Program
 {
-    private static void Main(string[] args)
+    private static readonly Dictionary<TokenType, Regex> TokenRegexes = new()
+    {
+        { TokenType.Space, RxSpace() },
+        { TokenType.Comment, RxComment() },
+        { TokenType.Keyword, RxKeyword() },
+        { TokenType.Number , RxNumber() },
+        { TokenType.Operator, RxOperator() },
+        { TokenType.Delimiter, RxDelimiter() },
+        { TokenType.Identifier, RxIdentifier() },
+    };
+    
+    private static void Main()
     {
         var input = 
             """
@@ -12,53 +23,43 @@ internal class Program
             2 mod 3 * hello
             """;
 
-        input = input.Trim();
-        input += '\n';
         while (input.Length > 0)
         {
-            var index = 1;
-            while (true)
+            foreach (var kvp in TokenRegexes)
             {
-                var token = input[..index];
-                if (TryMatchToken(token))
-                {
-                    input = input[index..].TrimStart();
-                    break;
-                }
-                index++;
+                var match = kvp.Value.Match(input);
+                if (!match.Success)
+                    continue;
+
+                if (kvp.Key != TokenType.Comment && kvp.Key != TokenType.Space)
+                    Console.WriteLine($"{kvp.Key}: {match.Value}");
                 
-                if (index > input.Length)
-                    return;
+                input = input[match.Length..];
+                break;
             }
         }
     }
 
-    private static bool TryMatchToken(string token)
-    {
-        Dictionary<TokenType, string> regexes = new()
-        {
-            { TokenType.Keyword, @"(div|mod) " },
-            { TokenType.Number, @"\d+ " },
-            { TokenType.Operator, @"^[+*\/] |-" },
-            { TokenType.Delimiter, @"[();]" },
-            { TokenType.Identifier, @"[a-zA-Z][a-zA-Z\d]*( |\n)" },
-            { TokenType.Comment, @"\/\/.*\n" },
-        };
-
-        foreach (var kvp in regexes)
-        {
-            var regex = new Regex(kvp.Value);
-            var match = regex.Match(token);
-            if (!match.Success)
-                continue;
-
-            if (kvp.Key != TokenType.Comment)
-                Console.WriteLine($"{kvp.Key}: {match.Value}");
-            return true;
-        }
-
-        return false;
-    }
+    [GeneratedRegex(@"^[a-zA-Z][a-zA-Z\d]*")]
+    private static partial Regex RxIdentifier();
+    
+    [GeneratedRegex(@"^\d+")]
+    private static partial Regex RxNumber();
+    
+    [GeneratedRegex(@"^[+\-*\/]")]
+    private static partial Regex RxOperator();
+    
+    [GeneratedRegex(@"^[();]")]
+    private static partial Regex RxDelimiter();
+    
+    [GeneratedRegex(@"^(div|mod)\b")]
+    private static partial Regex RxKeyword();
+    
+    [GeneratedRegex(@"^\/\/.*")]
+    private static partial Regex RxComment();
+    
+    [GeneratedRegex(@"^\s+")]
+    private static partial Regex RxSpace();
 }
 
 internal enum TokenType
@@ -69,4 +70,5 @@ internal enum TokenType
     Delimiter,
     Keyword,
     Comment,
+    Space,
 }
